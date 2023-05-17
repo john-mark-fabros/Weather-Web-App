45fea2e9c80ac920ea1ed3a675a4de5c7b90ad1f
John Mark Fabros <jmaking12345@gmail.com>
2023-05-17 17:48:50 +0800

    feat: refactor put function for validating null and empty input

--- backend/src/main/java/com/example/fullstackproject/backend/entity/Place.java
+++ backend/src/main/java/com/example/fullstackproject/backend/entity/Place.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
